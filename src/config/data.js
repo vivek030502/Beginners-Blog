d7ede03483e8857fa10bb680d57f3666da3eeb8e
vivek63 <110586449+vivek030502@users.noreply.github.com>
2024-09-17 21:28:34 +0530

    initial commit

---
+++ src/config/data.js
@@ -0,0 +1,95 @@
+/* eslint-disable no-dupe-keys */
+export const blogList = [
+  {
+    id: 2,
+    title: '7 CSS Tools You Should Be Using',
+    category: 'travel',
+    subCategory: ['vacation', 'holidays', 'sight seeing'],
+    description:
+      "CSS is getting more and more complicated to write. Thankfully, there are tools that make the task of authoring CSS a little bit easier. From identifying errors in your stylesheets to presenting useful stats about your code, these online tools will undoubtedly help during CSS development.",
+    authorName: 'John Doe',
+    authorAvatar: '/assets/images/author.jpg',
+    createdAt: 'June 03, 2022',
+    cover: '/assets/images/f67396fc3cfce63a28e07ebb35d974ac.jpg',
+  },
+  {
+    id: 2,
+    title: 'Milan Places That Highlight The City',
+    category: 'travel',
+    subCategory: ['vacation', 'holidays', 'sight seeing'],
+    description:
+      "Nowadays the Castello Sforzessco, the city’s former fifteenth-century defensive fortress, is now a place where art can be relished in several first-class museums. The most prized artworks are certainly Leonardo's Sala delle Asse and Michelangelo's Pietà Rondanini, but many other great Masters are represented. The Castello is also a place to enjoy in the open, pausing to take in the spectacular courtyards, embraced by the massive walls.",
+    authorName: 'Dan Ferndwark',
+    authorAvatar: '/assets/images/author.jpg',
+    createdAt: 'June 13, 2024',
+    cover: '/assets/images/f67396fc3cfce63a28e07ebb35d974ac.jpg',
+  },
+  {
+    id: 3,
+    title: 'Online Shopping – An Alternative to Shopping in the Mall',
+    category: 'shopping',
+    subCategory: ['e-commerce store', 'clothing', 'shopping store'],
+    description:
+      "Online shopping is a form of electronic commerce which allows consumers to directly buy goods or services from a seller over the Internet using a web browser or a mobile app. Consumers find a product of interest by visiting the website of the retailer directly or by searching among alternative vendors using a shopping search engine, which displays the same product's availability and pricing at different e-retailers. As of 2020, customers can shop online using a range of different computers and devices, including desktop computers, laptops, tablet computers and smartphones.",
+    authorName: 'Jane Deew',
+    authorAvatar: '/assets/images/author.jpg',
+    createdAt: 'June 15, 2023',
+    cover: '/assets/images/fQwuyKJ9qxjSbr6REcgtmW-1200-80.jpg',
+  },
+  {
+    id: 4,
+    title: 'ADVENTURE IN YOU',
+    category: 'adventure',
+    subCategory: ['adrenaline', 'stay-fit', 'lifestyle'],
+    description:
+      "We’re Tom and Anna. We’ve been on the road for the last ten years and are passionate about helping people create unforgettable memories.",
+    authorName: 'Steave Doggle ',
+    authorAvatar: '/assets/images/author.jpg',
+    createdAt: 'June 03, 2021',
+    authorAvatar: '/assets/images/author.jpg',
+    createdAt: 'june 08, 2024',
+    cover: '/assets/images/graphic-design-trends.png',
+  },
+  {
+    id: 5,
+    title: 'Loaded BBQ Baked Potatoes',
+    category: 'cooking',
+    subCategory: ['bbq', 'food', 'lifestyle'],
+    description:
+      "Grab some pork from your favorite local barbecue shop for these over-the-top potatoes.",
+    authorName: 'John Dewan',
+    authorAvatar: '/assets/images/author.jpg',
+    createdAt: 'Sep 05, 2024',
+    cover: '/assets/images/make-it-personal.jpg',
+  },
+  {
+    id: 6,
+    title: 'Beyond the Beach',
+    category: 'travel',
+    subCategory: ['beaches', 'sea', 'holidays'],
+    description:
+      "A powerful story of hope narrated through the lives of medical professionals on the ground as they bravely confront war's terrors and civilian woes fleeing from terror in an ISIS overrun Northern Iraq, brutal violence in Afghanistan before Kabul's now infamous international evacuation, through to the tragic migrant humanitarian crisis in the Mediterranean. An enduring story of humanity's resilience and compassion..",
+    authorName: 'fanward shekar',
+    authorAvatar: '/assets/images/author.jpg',
+    createdAt: 'July 05, 2024',
+    cover:
+      '/assets/images/Purple-Combination-colors-graphic-design-predictions-1024x576-1024x576.jpg',
+  },
+  {
+    id: 7,
+    title: 'Art & Perception',
+    category: 'art',
+    subCategory: ['skill', 'design', 'passion'],
+    description:
+      "Art & Perception will serve those across several areas of science studying the way works of art and design affect us perceptually, cognitively, or physiologically. The editors are also keen to receive submissions from practicing artists, and those in related fields of history and theory, which offer an artistic perspective on perception.",
+    authorName: 'John Doe',
+    authorAvatar: '/assets/images/author.jpg',
+    createdAt: 'Aug 10, 2024',
+    cover: '/assets/images/Synthwave-Postmodern.jpg',
+  },
+];
+// eslint-disable-next-line no-unused-vars
+function newFunction() {
+  return "CSS is getting more and more complicated to write. Thankfully, there are tools that make the task of authoring CSS a little bit easier. From identifying errors in your stylesheets to presenting useful stats about your code, these online tools will undoubtedly help during CSS development.";
+}
+
